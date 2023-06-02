a60a71c7ead0d6f85ad4d71a94e42e42297599fc
srquike <srquike@outlook.com>
2023-06-02 15:44:56 -0600

    Agregar componentes para autorizacion

---
+++ src/app/security/authorizer/authorizer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authorizer',
+  templateUrl: './authorizer.component.html',
+  styleUrls: ['./authorizer.component.css']
+})
+export class AuthorizerComponent {
+
+}
